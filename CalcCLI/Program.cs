e921382151aa77802b1e70dfd89e6dedff1fe983
kpfuse <kpfusps2021@gmail.com>
2025-04-21 16:36:31 +0300

    added a simple calculator

---
+++ CalcCLI/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Простой калькулятор");
+Console.Write("x = ");
+float x = float.Parse(Console.ReadLine() ?? "0.0f");
+Console.Write("y = ");
+float y = float.Parse(Console.ReadLine() ?? "0.0f");
+
+Console.Write("op : ");
+char op = (Console.ReadLine() ?? " ")[0];
+
+float result = op switch
+{
+    '+' => x + y,
+    '-' => x - y,
+    '*' => x * y,
+    _ => throw new Exception("Invalid op")
+};
+Console.WriteLine($"{x} {op} {y} = {result}");
