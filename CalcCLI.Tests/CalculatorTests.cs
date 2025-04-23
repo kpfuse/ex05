9bbe1fd2cc50674227055ed9c1782b8f63c93660
kpfuse <kpfusps2021@gmail.com>
2025-04-23 07:55:47 +0300

    added tests + - *

---
+++ CalcCLI.Tests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿namespace CalcCLI.Tests;
+
+public class CalculatorTests
+{
+    private readonly Calculator _calc = new Calculator();
+
+    [Theory]
+    [InlineData(3.5f, 2.5f, 6.0f)]
+    [InlineData(-1f, 1f, 0f)]
+    public void Add_ReturnsCorrectResult(float a, float b, float expected)
+    {
+        Assert.Equal(expected, _calc.Add(a, b), precision: 7);
+    }
+    //----------------------
+    // TO DO
+    //----------------------
+     [Fact]
+        public void Calculate_ThrowsException_OnInvalidOperator()
+        {
+            Assert.Throws<ArgumentException>(() => _calc.Calculate(1, 2, '>'));
+        }
+}
